660e97307386bca253e94e8a0dd90ad63cae88a0
Sam <sammy5673@hotmail.com>
2024-05-03 00:17:45 -0500

    Implemented LAN multiplayer

--- Piece.py
+++ Piece.py
@@ -1,8 +1,10 @@
 from board_utils import *
 
 class Piece(object):
-    def __init__(self, name, team, position, movable):
+    def __init__(self, name, team, position, moveable):
         self.name = name
-        self.team = team
-        self.position = position
-        self.movable = movable
+        self.team = team  # relative team information: 0 = opponent, 1 = friendly
+        #self.position = position
+        self.moveable = moveable
+        if self.name == FLAG:
+            self.do_display = False
